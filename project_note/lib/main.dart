32b1c96fdebd5961ddc6a77c29f447ae1f3e6449
Ali-10011 <98711622+Ali-10011@users.noreply.github.com>
2022-09-08 03:08:53 +0500

    Served Auth requests from client-server

--- project_note/lib/main.dart
+++ project_note/lib/main.dart
@@ -1,5 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-import 'package:web_socket_channel/io.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -18,67 +20,118 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
+
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String? _message;
-
-  // This function will send the message to our backend.
-  void sendMessage(msg) {
-    IOWebSocketChannel? channel;
-    // We use a try - catch statement, because the connection might fail.
-    try {
-      // Connect to our backend.
-      channel = IOWebSocketChannel.connect('ws://localhost:3000'); //making connection
-    } catch (e) {
-      // If there is any error that might be because you need to use another connection.
-      print("Error on connecting to websocket: " + e.toString());
-    }
-    // Send message to backend
-    channel?.sink.add(msg); //for requesting data
-
-    // Listen for any message from backend
-    channel?.stream.listen((event) { //for listening response
-      // Just making sure it is not empty
-      if (event!.isNotEmpty) {
-        print(event);
-        // Now only close the connection and we are done here!
-        channel!.sink.close(); //we received data and now terminating conection
-      }
-    });
-  }
-
+  final TextEditingController _emailcontroller = TextEditingController();
+  final TextEditingController _passwordcontroller = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Column(
-          children: [
-            Expanded(
-              child: Center(
-                child: TextField(
-                  onChanged: (e) => _message = e,
-                ),
+    return SafeArea(
+      child: Scaffold(
+        body: Column(mainAxisAlignment: MainAxisAlignment.center, children: <
+            Widget>[
+          Container(
+            width: MediaQuery.of(context).size.width * 0.5,
+            child: TextFormField(
+              controller: _emailcontroller,
+              keyboardType: const TextInputType.numberWithOptions(),
+              // inputFormatters: [
+              //   FilteringTextInputFormatter.allow(RegExp(
+              //       r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")),
+              // ],
+              decoration: const InputDecoration(
+                icon: Icon(Icons.person),
+                hintText: 'JohnDoe@gmail.com',
+                labelText: 'Email',
               ),
+              onSaved: (String? value) {
+                // This optional block of code can be used to run
+                // code when the user saves the form.
+              },
+              // validator: (String? value) {
+              //   return (value != null && value.contains('@'))
+              //       ? 'Do not use the @ char.'
+              //       : null;
+              // },
             ),
-            Expanded(
-              child: Center(
-                child: TextButton(
-                  child: const Text("Send"),
-                  onPressed: () {
-                    // Check if the message isn't empty.
-                    if (_message!.isNotEmpty) {
-                      sendMessage(_message);
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width * 0.5,
+            child: TextFormField(
+              controller: _passwordcontroller,
+              decoration: const InputDecoration(
+                icon: Icon(Icons.person),
+                hintText: 'I_amAlive',
+                labelText: 'Password',
+              ),
+              onSaved: (String? value) {
+                // This optional block of code can be used to run
+                // code when the user saves the form.
+              },
+              // validator: (String? value) {
+              //   return (value != null && value.contains('@'))
+              //       ? 'Do not use the @ char.'
+              //       : null;
+              // },
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                  onPressed: () async {
+                    try {
+                      var response = await http
+                          .post(Uri.parse('http://localhost:3000/'), headers: {
+                        "Content-Type": "application/x-www-form-urlencoded"
+                      }, body: {
+                        'authtype': 'login',
+                        'username': _emailcontroller.value.text.toString(),
+                        'password': _passwordcontroller.value.text.toString()
+                      });
+
+                      if (response.statusCode == 200) {
+                        print(response.body);
+                      } else {
+                        print(json.decode(response.body));
+                      }
+                    } catch (e) {
+                      print(e.toString());
                     }
                   },
-                ),
+                  child: const Text('Log in')),
+              SizedBox(
+                width: 10,
               ),
-            ),
-          ],
-        ),
+              ElevatedButton(
+                  onPressed: () async {
+                    try {
+                      var response = await http
+                          .post(Uri.parse('http://localhost:3000/'), headers: {
+                        "Content-Type": "application/x-www-form-urlencoded"
+                      }, body: {
+                        'authtype': 'signup'.toString(),
+                        'username': _emailcontroller.value.text.toString(),
+                        'password': _passwordcontroller.value.text.toString()
+                      });
+
+                      if (response.statusCode == 200) {
+                        print(response.body);
+                      } else {
+                        print(json.decode(response.body));
+                      }
+                    } catch (e) {
+                      print(e.toString());
+                    }
+                  },
+                  child: const Text('Sign up'))
+            ],
+          ),
+        ]),
       ),
     );
   }
